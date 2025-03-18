b8b09b0103547ddbbb4786e3c3e42d9a5f18149d
Marmik Modi <marmikmodi209@gmail.com>
2025-03-18 16:35:26 +0530

    Replace Immutable Set with Java Util Set.of() to solve the NoClassDefFoundError

--- src/main/java/com/crisil/trino/plugin/EncryptDecryptPlugin.java
+++ src/main/java/com/crisil/trino/plugin/EncryptDecryptPlugin.java
@@ -4,23 +4,23 @@ import com.crisil.trino.udf.EncryptDecryptUDF;
 import com.crisil.trino.udf.ValidateDate;
 import com.crisil.trino.udf.ValidateMobileNo;
 import com.crisil.trino.udf.ValidatePAN;
-import com.google.common.collect.ImmutableSet;
 import io.trino.spi.Plugin;
 import io.trino.spi.function.FunctionProvider;
 
 import java.util.Set;
 
-    public class EncryptDecryptPlugin
-            implements Plugin, FunctionProvider
+public class EncryptDecryptPlugin
+        implements Plugin, FunctionProvider
+{
+    @Override
+    public Set<Class<?>> getFunctions()
     {
-        @Override
-        public Set<Class<?>> getFunctions()
-        {
-            return ImmutableSet.<Class<?>>builder()
-                    .add(EncryptDecryptUDF.class)
-                    .add(ValidatePAN.class)
-                    .add(ValidateMobileNo.class)
-                    .add(ValidateDate.class)
-                    .build();
-        }
+        return Set.of(
+                EncryptDecryptUDF.class,
+                ValidatePAN.class,
+                ValidateMobileNo.class,
+                ValidateDate.class
+        );
+
     }
+}
